403ab5d2d458a2b1fb48b01364d8f33338141b49
EloK6 <ecassignol@gmail.com>
2019-01-31 20:24:20 +0100

    big merge

--- models/Project.js
+++ models/Project.js
@@ -1,19 +1,33 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const projectSchema = new Schema({
-  name: String,
-  owner : [{ type: Schema.ObjectId, ref: "User" }],
-  category : { type : string, enum : ["Front-end","Back-end","FullStack","UX design","Data Analyst"]},
-  summary : String,
-  skills : [{ type: Schema.ObjectId, ref: "Skill" }],
-  duration : { type : string, enum : ["Moins d'un mois", "D'un à trois mois", "De trois à six mois", "Plus de six mois"]},
-}, {
-  timestamps: {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at'
-  }
-});
+const projectSchema = new Schema(
+	{
+		name: String,
+		owner: [{ type: Schema.ObjectId, ref: "User" }],
+		category: {
+			type: String,
+			enum: ["Front-end", "Back-end", "FullStack", "UX design", "Data Analyst"]
+		},
+		summary: String,
+		skills: [{ type: Schema.ObjectId, ref: "Skill" }],
+		duration: {
+			type: String,
+			enum: [
+				"Moins d'un mois",
+				"D'un à trois mois",
+				"De trois à six mois",
+				"Plus de six mois"
+			]
+		}
+	},
+	{
+		timestamps: {
+			createdAt: "created_at",
+			updatedAt: "updated_at"
+		}
+	}
+);
 
-const Project = mongoose.model('Project', projectSchema);
+const Project = mongoose.model("Project", projectSchema);
 module.exports = Project;
--- routes/auth.js
+++ routes/auth.js
@@ -1,64 +1,71 @@
 const express = require("express");
-const passport = require('passport');
+const passport = require("passport");
 const router = express.Router();
 const User = require("../models/User");
+const ensureLogin = require("connect-ensure-login");
 
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
 
 router.get("/login", (req, res, next) => {
-  res.render("auth/login", { "message": req.flash("error") });
+	res.render("auth/login", { message: req.flash("error") });
 });
 
-router.post("/login", passport.authenticate("local", {
-  successRedirect: "/projects",
-  failureRedirect: "/badlogin",
-  failureFlash: true,
-  passReqToCallback: true
-}));
+router.post(
+	"/login",
+	passport.authenticate("local", {
+		successRedirect: "/profile/edit",
+		failureRedirect: "/badlogin",
+		failureFlash: true,
+		passReqToCallback: true
+	})
+);
 
 router.get("/signup", (req, res, next) => {
-  res.render("auth/signup");
+	res.render("auth/signup");
 });
 
 router.post("/signup", (req, res, next) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  const accountType = req.body.accountType;
-  if (email === "" || password === "") {
-    res.render("auth/signup", { message: "Indicate email and password" });
-    return;
-  }
+	const email = req.body.email;
+	const password = req.body.password;
+	const accountType = req.body.accountType;
+	console.log("accountType", accountType);
+	if (email === "" || password === "") {
+		res.render("auth/signup", { message: "Indicate email and password" });
+		return;
+	}
 
-  User.findOne({ email }, "email", (err, user) => {
-    if (user !== null) {
-      res.render("auth/signup", { message: "The email already exists" });
-      return;
-    }
+	User.findOne({ email }, "email", (err, user) => {
+		if (user !== null) {
+			res.render("auth/signup", { message: "The email already exists" });
+			return;
+		}
 
-    const salt = bcrypt.genSaltSync(bcryptSalt);
-    const hashPass = bcrypt.hashSync(password, salt);
+		const salt = bcrypt.genSaltSync(bcryptSalt);
+		const hashPass = bcrypt.hashSync(password, salt);
 
-    const newUser = new User({
-      email,
-      password: hashPass,
-      accountType
-    });
+		const newUser = new User({
+			email,
+			password: hashPass,
+			accountType
+		});
 
-    newUser.save()
-    .then(() => {
-      res.redirect("/");
-    })
-    .catch(err => {
-      res.render("auth/signup", { message: "Something went wrong" });
-    })
-  });
+		newUser
+			.save()
+			.then(() => {
+				res.redirect("/auth/login");
+			})
+			.catch(err => {
+				console.log(err);
+				res.render("auth/signup", { message: "Something went wrong" });
+			});
+	});
 });
 
 router.get("/logout", (req, res) => {
-  req.logout();
-  res.redirect("/");
+	req.logout();
+	res.redirect("/");
 });
 
 module.exports = router;
