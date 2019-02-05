2da460b75e40acc976f0e6382fa8fcc3eaefa5d1
EloK6 <ecassignol@gmail.com>
2019-02-05 20:04:25 +0100

    merge

--- routes/profiles.js
+++ routes/profiles.js
@@ -4,12 +4,13 @@ const User = require("../models/User");
 const ensureLogin = require("connect-ensure-login");
 const uploadCloud = require("../config/cloudinary.js");
 
-//User edit his profile
+//User edit its profile
 router.get(
-  "/profile/edit/:id",
+  "/profile/edit",
   ensureLogin.ensureLoggedIn("/auth/login"),
+  uploadCloud.single("photo"),
   (req, res, next) => {
-    User.findOne({ _id: req.params.id })
+    User.findOne({ _id: req.user.id })
       .then(profileEdit => {
         res.render("profile/edit", { profileEdit });
       })
@@ -19,71 +20,46 @@ router.get(
   }
 );
 
-//User edit his photo
-router.post(
-  "profile/edit/:id",
-  ensureLogin.ensureLoggedIn("/auth/login"),
-  uploadCloud.single("photo"),
-  (req, res, next) => {
-    User.findOne({ _id: req.params.id });
-    const image = req.file.url;
-    const profileUpdated = new User(image);
-    newUser
-      .save()
-      .then(profileEdit => {
-        res.render("profile/edit", { profileEdit });
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-);
-
-router.post(
-  "/projects/edit/:id",
-  ensureLogin.ensureLoggedIn("/auth/login"),
-  uploadCloud.single("photo"),
-  (req, res, next) => {
-    const {
-      email,
-      password,
-      firstname,
-      lastname,
-      companyName,
-      skills,
-      lastJob,
-      experiences,
-      resume,
-      linkedinProfile
-    } = req.body;
-    const image = req.file.url;
-    Project.update(
-      { _id: req.params.id },
-      {
-        $set: {
-          email,
-          password,
-          firstname,
-          lastname,
-          companyName,
-          image,
-          skills,
-          lastJob,
-          experiences,
-          resume,
-          linkedinProfile
-        }
-      },
-      { new: true }
-    )
-      .then(project => {
-        res.redirect("/projects/" + req.params.id);
-      })
-      .catch(error => {
-        next(error);
-      });
-  }
-);
+router.post("/projects/edit/:id", (req, res, next) => {
+  const {
+    email,
+    password,
+    firstname,
+    lastname,
+    companyName,
+    image,
+    skills,
+    lastJob,
+    experiences,
+    resume,
+    linkedinProfile
+  } = req.body;
+  Project.update(
+    { _id: req.params.id },
+    {
+      $set: {
+        email,
+        password,
+        firstname,
+        lastname,
+        companyName,
+        image,
+        skills,
+        lastJob,
+        experiences,
+        resume,
+        linkedinProfile
+      }
+    },
+    { new: true }
+  )
+    .then(project => {
+      res.redirect("/projects/" + req.params.id);
+    })
+    .catch(error => {
+      next(error);
+    });
+});
 
 //Enterprise see all the ironhackers
 router.get(
