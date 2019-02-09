debbe8a518e82451ddef9594a0f13a97e191a789
EloK6 <ecassignol@gmail.com>
2019-02-09 14:26:34 +0100

    Edit projects OK

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -16,10 +16,24 @@ $("#deleteProjectModal").on("show.bs.modal", function(e) {
 //Edit Projects
 $("#editProjectModal").on("show.bs.modal", function(e) {
   let projectIdEdit = $(e.relatedTarget).data("projectidedit");
+  $("#editProjectModal form").attr("action", `/my-projects/${projectIdEdit}`);
   axios
     .get(`/my-projects/${projectIdEdit}`)
     .then(res => {
-      console.log("res", res);
+      $("#name").val(res.data.name);
+      $("#summary").val(res.data.summary);
+      $("[name='category']").prop("checked", false);
+      res.data.category.forEach(category => {
+        $(`input[value='${category}']`).prop("checked", true);
+      });
+      $("[name='duration']").prop("checked", false);
+      res.data.duration.forEach(duration => {
+        $(`input[value='${duration}']`).prop("checked", true);
+      });
+      $("[name='skills']").prop("checked", false);
+      res.data.skills.forEach(skills => {
+        $(`input[value='${skills}']`).prop("checked", true);
+      });
     })
     .catch(err => {
       // Here we catch the error and display it
