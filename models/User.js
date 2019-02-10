5a9550229a01b25605c7d5dabf6f21fa505e788c
EloK6 <ecassignol@gmail.com>
2019-02-10 18:44:28 +0100

    test sidebar

--- models/User.js
+++ models/User.js
@@ -6,6 +6,7 @@ const userSchema = new Schema(
     email: String,
     password: String,
     firstname: String,
+    username: String,
     lastname: String,
     accountType: { type: String, enum: ["ironhacker", "enterprise"] },
     companyName: {
